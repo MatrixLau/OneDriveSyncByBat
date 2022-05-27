2c8fd109114e0f34fa329063ae7498d15489dc2c
MatrixLau <matrixlj0113@gmail.com>
2022-05-27 10:35:54 +0800

    Revert "Update Sync logic will do REALLY SYNC NOW"
    This reverts commit 9a18236865c6bbf99b18260c77f4940df97e1c74.

--- OneDriveSync.bat
+++ OneDriveSync.bat
@@ -1,7 +1,8 @@
 @echo off
 @REM 若OneDrive没有设置环境变量请手动设置OneDrive位置
 set ONEDRIVEPATH=C:\OneDrive
-set FILENAME=OneDriveSycn_process.txt
+set FILENAME=OneDriveSycn_process
+set FILETYPE=.txt
 @REM echo %ONEDRIVEPATH%
 
 if not exist %OneDrive% (
@@ -13,11 +14,12 @@ if not exist %OneDrive% (
 
 :not_found_onedrive
 echo "PLEASE SET UP YOUR ONEDRIVE FIRST!!!"
+pause
 goto :EOF
 
 :pre_process
 set ONEDRIVEPATH=%OneDrive%
-set ONEDRIVESYNCFILE=%ONEDRIVEPATH%\%FILENAME%
+set ONEDRIVESYNCFILE=%ONEDRIVEPATH%\%FILENAME%%FILETYPE%
 goto process
 
 :process
@@ -29,16 +31,28 @@ if exist %ONEDRIVESYNCFILE% (
 
 :create_file
 md %ONEDRIVESYNCFILE%
-goto write_file
+goto rename_file
 
 :check_file
 @REM set ONEDRIVESYNCFILECONTENT=type E:\OneDrive\OneDriveSycn_process.txt
 @REM type E:\OneDrive\OneDriveSycn_process.txt > %ONEDRIVESYNCFILECONTENT%
 @REM if %ONEDRIVESYNCFILECONTENT% == %date%%time% ( echo "NOT SO HURRY!" ) else ( goto write_file )
-goto write_file
+goto rename_file
 goto :EOF
 
-:write_file
-echo %date%%time% > %ONEDRIVESYNCFILE%
+@REM :write_file
+@REM echo %date%%time% > %ONEDRIVESYNCFILE%
+@REM echo Sycning...
+@REM goto :EOF
+
+:rename_file
+for %%a in ( %ONEDRIVEPATH%\* ) do (
+    set FINDFILE=%%a
+    setLocal EnableDelayedExpansion
+    if not "x!FINDFILE:%FILENAME%=!"=="x!FINDFILE!" (
+        ren "%%a" "%FILENAME%%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%%FILETYPE%"
+    )
+    endlocal
+)
 echo Sycning...
 goto :EOF
