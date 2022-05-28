0186b830d04480f17ccecf70a0ea51c9765dbbf8
MatrixLau <matrixlj0113@gmail.com>
2022-05-28 17:17:33 +0800

    Fixed logic

--- OneDriveSync.bat
+++ OneDriveSync.bat
@@ -1,12 +1,14 @@
-@REM @echo off
+@echo off
+setLocal EnableDelayedExpansion
 
-set ONEDRIVEPATH=YOURONEDRIVEPATH
-set FILENAME=OneDriveSycn_process
+@REM 若OneDrive没有设置环境变量请手动设置OneDrive位置
+set ONEDRIVEPATH=C:\OneDrive
+set FILENAME=OneDriveSync_process
 set FILETYPE=.txt
-@REM echo %ONEDRIVEPATH%
 
 if not exist %OneDrive% (
-    goto not_found_onedrive
+    if exist %ONEDRIVEPATH% ( goto process )
+    else ( goto not_found_onedrive )
 ) else (
     goto pre_process
 )
@@ -22,36 +24,19 @@ set ONEDRIVESYNCFILE=%ONEDRIVEPATH%\%FILENAME%%FILETYPE%
 goto process
 
 :process
-if exist %ONEDRIVESYNCFILE% ( 
-    goto check_file 
-) else ( 
-    goto create_file
-)
-
-:create_file
-echo "" > %ONEDRIVESYNCFILE%
 goto rename_file
-
-:check_file
-@REM set ONEDRIVESYNCFILECONTENT=type E:\OneDrive\OneDriveSycn_process.txt
-@REM type E:\OneDrive\OneDriveSycn_process.txt > %ONEDRIVESYNCFILECONTENT%
-@REM if %ONEDRIVESYNCFILECONTENT% == %date%%time% ( echo "NOT SO HURRY!" ) else ( goto write_file )
-goto rename_file
-goto :EOF
-
-@REM :write_file
-@REM echo %date%%time% > %ONEDRIVESYNCFILE%
-@REM echo Sycning...
-@REM goto :EOF
 
 :rename_file
+set /a FINDFILECOUNT=0
 for %%a in ( %ONEDRIVEPATH%\* ) do (
     set FINDFILE=%%a
-    setLocal EnableDelayedExpansion
     if not "x!FINDFILE:%FILENAME%=!"=="x!FINDFILE!" (
+        set /a FINDFILECOUNT+=1
         ren "%%a" "%FILENAME%%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%%FILETYPE%"
     )
-    endlocal
 )
-echo Sycning...
+if !FINDFILECOUNT! == 0 (
+    echo "" > %ONEDRIVESYNCFILE%
+)
+echo Syncing...
 goto :EOF
